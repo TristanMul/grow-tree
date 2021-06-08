d894403e45f24040107600c73db907452d36f91b
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-06-08 14:15:02 +0200

    Crosshair + Vertical movement for crosshair
    - Crosshair added
    - Vertical movement for crosshair with clamp

---
+++ Grow Tree/Assets/Scripts/Slingshot/CrosshairMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairMovement : MonoBehaviour
+{
+    [SerializeField] private FloatValue verticalValue;
+    [SerializeField] private Vector3Value playerPosition;
+    [SerializeField] private bool canMove;
+    [SerializeField] private float sensitivity;
+    private Rigidbody rigidBody;
+    private Vector3 myPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPos = GetComponent<Transform>().position;
+        rigidBody = GetComponent<Rigidbody>();
+        playerPosition.Value = transform.position;
+    }
+
+    private void Update()
+    {
+        //playerPosition.Value = transform.position;
+        transform.position = new Vector3(transform.parent.position.x, transform.position.y, transform.position.z);
+        //myPos.x = transform.parent.position.x;
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!canMove)
+        {
+            rigidBody.velocity = Vector3.zero;
+            return;
+        }
+
+        float clampYPosition;
+        rigidBody.velocity = new Vector3(0f ,verticalValue.Value * sensitivity , 0f);
+        clampYPosition = Mathf.Clamp(transform.position.y, 0.5f, 6.9f);
+        transform.position = new Vector3(transform.position.x, clampYPosition, transform.position.z);
+    }
+
+    public void PlayerCanMove(bool canMove)
+    {
+        this.canMove = canMove;
+    }
+}
