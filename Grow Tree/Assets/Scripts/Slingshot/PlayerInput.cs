34c370865a8f2f50349b570a3afc2e2142213695
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-06-08 13:41:41 +0200

    Input manager now detects X and Y axis

--- Grow Tree/Assets/Scripts/Slingshot/PlayerInput.cs
+++ Grow Tree/Assets/Scripts/Slingshot/PlayerInput.cs
@@ -6,12 +6,20 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private LayerMask hitLayers;
     [SerializeField] private FloatValue horizontalSpeed;
+    [SerializeField] private FloatValue verticalSpeed;
     [SerializeField] private float horizontalSpeedCap;
+    [SerializeField] private float verticalSpeedCap;
     [SerializeField] private float dragSpeed;
 
     private Vector3 previousMousePosition;
     private Vector3 currentMousePosition;
-    
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
     void Update()
     {
         UpdatePosition();
@@ -20,26 +28,33 @@ public class PlayerInput : MonoBehaviour
     private void UpdatePosition()
     {
         Vector3 mousePosition;
-        float deltaMousePosition;
-        float sign;
-        float speed;
+        Vector2 _speed;
+        Vector2 _deltaMousePosition;
+        Vector2 sign;
 
         if (Input.GetMouseButtonDown(0))
         {
-            mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z + 2f);
-            previousMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.transform.position.z + 2f);
+            previousMousePosition = camera.ScreenToWorldPoint(mousePosition);
         }
         else if (Input.GetMouseButton(0))
         {
-            mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z + 2f);
-            currentMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.transform.position.z + 2f);
+            currentMousePosition = camera.ScreenToWorldPoint(mousePosition);
 
-            deltaMousePosition = Mathf.Abs(previousMousePosition.x - currentMousePosition.x);
-            sign = Mathf.Sign(previousMousePosition.x - currentMousePosition.x);
+            _deltaMousePosition.x = Mathf.Abs(previousMousePosition.x - currentMousePosition.x);
+            sign.x = Mathf.Sign(previousMousePosition.x - currentMousePosition.x);
 
-            speed = deltaMousePosition * dragSpeed * -sign;
-            speed = Mathf.Clamp(speed, -horizontalSpeedCap, horizontalSpeedCap);
-            horizontalSpeed.Value = speed;
+            _speed.x = _deltaMousePosition.x * dragSpeed * -sign.x;
+            _speed.x = Mathf.Clamp(_speed.x, -horizontalSpeedCap, horizontalSpeedCap);
+            horizontalSpeed.Value = _speed.x;
+
+            _deltaMousePosition.y = Mathf.Abs(previousMousePosition.y - currentMousePosition.y);
+            sign.y = Mathf.Sign(previousMousePosition.y - currentMousePosition.y);
+
+            _speed.y = _deltaMousePosition.y * dragSpeed * -sign.y;
+            _speed.y = Mathf.Clamp(_speed.y, -verticalSpeedCap, verticalSpeedCap);
+            verticalSpeed.Value = _speed.y;
 
             previousMousePosition = currentMousePosition;
         }
