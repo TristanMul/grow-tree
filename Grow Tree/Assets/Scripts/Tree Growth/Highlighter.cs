373977a2cd62c7c2f57d75f26aecb93daeed63ad
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-06-10 14:19:00 +0200

    circle highlight

---
+++ Grow Tree/Assets/Scripts/Tree Growth/Highlighter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighter : MonoBehaviour
+{
+    [SerializeField] GameObject highlightCircle;
+    List<GameObject> circles = new List<GameObject>();
+    Camera cam;
+    public static Highlighter instance;
+    private void Start()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        Generator.instance.OnStartGrowing += ClearCircles;
+    }
+
+
+    public void AddCircleFromScreenPos(Vector3 position)
+    {
+        GameObject newCircle = Instantiate(highlightCircle, transform);
+        newCircle.transform.position = position;
+        circles.Add(newCircle);
+    }
+
+    public void AddCircleFromWorldPos(Vector3 position)
+    {
+        GameObject newCircle = Instantiate(highlightCircle, transform);
+        newCircle.transform.position = Camera.main.WorldToScreenPoint(position);
+        circles.Add(newCircle);
+    }
+
+    public void ClearCircles()
+    {
+        foreach(GameObject circle in circles)
+        {
+            Destroy(circle);
+        }
+        circles.Clear();
+    }
+}
