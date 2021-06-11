77f3d6abae6869875b5adc33acaea61563ad4579
Tristan <tristanmul@gmail.com>
2021-06-11 15:29:41 +0200

    Small performance improvement

--- Grow Tree/Assets/Scripts/CameraFollow.cs
+++ Grow Tree/Assets/Scripts/CameraFollow.cs
@@ -7,42 +7,38 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private VectorList activeAttractors;
     [SerializeField] private List<Vector3> _attractorsList;
     public Vector3 cameraOffset;
+    Vector3 newPos;
     public float cameraSmoothing;
     private Vector3 velocity;
     private List<Vector3> generator;
     private float interval = 1f;
-
+    private float timeSinceIteration = 0;
     void Start()
     {
-        
+
     }
-    
+
     void LateUpdate()
     {
-        Debug.Log(Generator.instance._extremities.Count);
-        //_attractorsList = activeAttractors.CurrentObjectList;
-        //for (int i = 0; i < Generator.instance._extremities.Count; i++)
-        //{
-        //    _attractorsList.Add(Generator.instance._extremities[i]._start);
-        //}
-        _attractorsList.Clear();
-        foreach (Generator.Branch extremedy in Generator.instance._extremities)
+        timeSinceIteration += Time.deltaTime;
+        if ( timeSinceIteration> Generator.instance._timeBetweenIterations)
         {
-            if (extremedy._canGrow)
+            _attractorsList.Clear();
+            foreach (Generator.Branch extremedy in Generator.instance._extremities)
             {
-                _attractorsList.Add(extremedy._start);
+                if (extremedy._canGrow)
+                {
+                    _attractorsList.Add(extremedy._start);
+                }
             }
-            //else
-            //{
-            //    _attractorsList.Remove(extremedy._start);
-            //}
-        }
 
-        if (_attractorsList.Count == 0)
-            return;
-        Vector3 centerPos = GetCenterPoint();
-        Vector3 newPos = centerPos + cameraOffset;
-        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, cameraSmoothing);
+            if (_attractorsList.Count == 0)
+                return;
+            Vector3 centerPos = GetCenterPoint();
+            newPos = centerPos + cameraOffset;
+            timeSinceIteration = 0;
+        }
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, cameraSmoothing);
     }
 
     Vector3 GetCenterPoint()
