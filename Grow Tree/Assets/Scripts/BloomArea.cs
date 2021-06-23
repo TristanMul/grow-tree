d4d501cf58046e46400fced0f52895e8a5c2e892
Tristan <tristanmul@gmail.com>
2021-06-23 09:10:23 +0200

    Revert "Revert "Merge branch 'Tristan' into main""
    This reverts commit 548f05da3993602fd8432c6d9ea9c931d52aecf7.

---
+++ Grow Tree/Assets/Scripts/BloomArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloomArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Branch"))
+        {
+            Debug.Log("Setting true");
+            Generator.instance._branches[int.Parse(other.name)].canBloom = true;
+        }
+    }
+}
--- Grow Tree/Assets/Scripts/FinishGame.cs
+++ Grow Tree/Assets/Scripts/FinishGame.cs
@@ -6,6 +6,7 @@ public class FinishGame : MonoBehaviour
 {
     [SerializeField] private GameEvent winGame;
     [SerializeField] private GameEvent loseGame;
+    private GameObject rockformation;
     [SerializeField] private GameObject[] flowers;
     [SerializeField] Generator generator;
     [SerializeField] private int flowerRatio;
@@ -15,29 +16,41 @@ public class FinishGame : MonoBehaviour
     bool coroutineActivated = false;
     private int growingBranches = 0;
 
+    public void Start()
+    {
+        rockformation = GameObject.Find("Rock formation");
+    }
     public void Test()
     {
         if (!coroutineActivated)
         {
             coroutineActivated = true;
             StartCoroutine(waitSeconds());
-           
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Branch"))
         {
-            
+<<<<<<< Updated upstream
+        winGame.Raise();
+        Highlighter.instance.ClearCircles();
+        generator._timeBetweenIterations = 0.05f;
+=======
+            foreach (Transform children in rockformation.transform)
+            {
+                children.transform.GetChild(0).GetComponentInChildren<Collider>().enabled = false;
+            }
+            generator.BranchHitBarrier();
             winGame.Raise();
             generator._timeBetweenIterations = 0.05f;
+>>>>>>> Stashed changes
         }
     }
 
     public IEnumerator CheckIfLost()
     {
-        yield return new WaitForSeconds(generator._timeBetweenIterations + 0.01f);
-
+        yield return new WaitForSeconds(0.5f);
         growingBranches = 0;
         foreach (Generator.Branch branch in generator._extremities)
         {
@@ -46,40 +59,45 @@ public class FinishGame : MonoBehaviour
                 growingBranches++;
             }
         }
-
-        if (growingBranches == 0 && Highlighter.instance.highlights.Count == 0 && winGame != null)
+        Debug.Log(growingBranches);
+        if (growingBranches == 0)
         {
-            yield return new WaitForSeconds(0.5f);
-            if (growingBranches == 0)
-            {
-                loseGame.Raise();
-            }
+            loseGame.Raise();
         }
     }
     public IEnumerator waitSeconds()
     {
+<<<<<<< Updated upstream
+=======
         yield return new WaitForSeconds(generator._timeBetweenIterations + 0.01f);
         Highlighter.instance.ClearCircles();
+>>>>>>> Stashed changes
+        yield return new WaitForSeconds(1f);
         generator.finishGrowing = true;
         StartCoroutine(growFlowers());
     }
     public IEnumerator growFlowers()
     {
 
-        for (int i=generator._branches.Count-1; i>=0; i--)
+        for (int i=/*generator._branches.Count-1*/generator._extremities.Count-1; i>=0; i--)
         {
-            if(generator._branches[i]._children.Count == 0)
+<<<<<<< Updated upstream
+            /*if(generator._branches[i]._children.Count == 0)
+            {*/
+=======
+            if(generator._branches[i]._children.Count == 0 && generator._branches[i].canBloom)
             {
+>>>>>>> Stashed changes
                 int randomNumber = Random.Range(clusterMin, clusterMax);
                 for (int j = 0; j < randomNumber; j++)
                 {
                     int randomItem = Random.Range(0, flowers.Length);
                     float x = Random.Range(-maxDeviation, maxDeviation);
                     float y = Random.Range(-maxDeviation, maxDeviation);
-                    Instantiate(flowers[randomItem], generator._branches[i]._end + new Vector3(x, y, -0.5f), new Quaternion(0, 0, 0, 0));
+                    Instantiate(flowers[randomItem], /*generator._branches[i]._end*/generator._extremities[i]._end + new Vector3(x, y, -0.5f), new Quaternion(0, 0, 0, 0));
                 }
-                yield return new WaitForSeconds(0.05f);
-            }
+                yield return new WaitForSeconds(0.01f);
+           /* }*/
         }
     }
 }
