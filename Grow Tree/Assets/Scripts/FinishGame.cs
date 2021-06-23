c75492975b8e00a399a51feb055bdcbe6efa2d5f
Tristan <tristanmul@gmail.com>
2021-06-23 09:10:37 +0200

    Revert "Revert "Revert "Merge branch 'Tristan' into main"""
    This reverts commit d4d501cf58046e46400fced0f52895e8a5c2e892.

--- Grow Tree/Assets/Scripts/FinishGame.cs
+++ Grow Tree/Assets/Scripts/FinishGame.cs
@@ -6,7 +6,6 @@ public class FinishGame : MonoBehaviour
 {
     [SerializeField] private GameEvent winGame;
     [SerializeField] private GameEvent loseGame;
-    private GameObject rockformation;
     [SerializeField] private GameObject[] flowers;
     [SerializeField] Generator generator;
     [SerializeField] private int flowerRatio;
@@ -16,41 +15,29 @@ public class FinishGame : MonoBehaviour
     bool coroutineActivated = false;
     private int growingBranches = 0;
 
-    public void Start()
-    {
-        rockformation = GameObject.Find("Rock formation");
-    }
     public void Test()
     {
         if (!coroutineActivated)
         {
             coroutineActivated = true;
             StartCoroutine(waitSeconds());
+           
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Branch"))
         {
-<<<<<<< Updated upstream
-        winGame.Raise();
-        Highlighter.instance.ClearCircles();
-        generator._timeBetweenIterations = 0.05f;
-=======
-            foreach (Transform children in rockformation.transform)
-            {
-                children.transform.GetChild(0).GetComponentInChildren<Collider>().enabled = false;
-            }
-            generator.BranchHitBarrier();
+            
             winGame.Raise();
             generator._timeBetweenIterations = 0.05f;
->>>>>>> Stashed changes
         }
     }
 
     public IEnumerator CheckIfLost()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(generator._timeBetweenIterations + 0.01f);
+
         growingBranches = 0;
         foreach (Generator.Branch branch in generator._extremities)
         {
@@ -59,45 +46,40 @@ public class FinishGame : MonoBehaviour
                 growingBranches++;
             }
         }
-        Debug.Log(growingBranches);
-        if (growingBranches == 0)
+
+        if (growingBranches == 0 && Highlighter.instance.highlights.Count == 0 && winGame != null)
         {
-            loseGame.Raise();
+            yield return new WaitForSeconds(0.5f);
+            if (growingBranches == 0)
+            {
+                loseGame.Raise();
+            }
         }
     }
     public IEnumerator waitSeconds()
     {
-<<<<<<< Updated upstream
-=======
         yield return new WaitForSeconds(generator._timeBetweenIterations + 0.01f);
         Highlighter.instance.ClearCircles();
->>>>>>> Stashed changes
-        yield return new WaitForSeconds(1f);
         generator.finishGrowing = true;
         StartCoroutine(growFlowers());
     }
     public IEnumerator growFlowers()
     {
 
-        for (int i=/*generator._branches.Count-1*/generator._extremities.Count-1; i>=0; i--)
+        for (int i=generator._branches.Count-1; i>=0; i--)
         {
-<<<<<<< Updated upstream
-            /*if(generator._branches[i]._children.Count == 0)
-            {*/
-=======
-            if(generator._branches[i]._children.Count == 0 && generator._branches[i].canBloom)
+            if(generator._branches[i]._children.Count == 0)
             {
->>>>>>> Stashed changes
                 int randomNumber = Random.Range(clusterMin, clusterMax);
                 for (int j = 0; j < randomNumber; j++)
                 {
                     int randomItem = Random.Range(0, flowers.Length);
                     float x = Random.Range(-maxDeviation, maxDeviation);
                     float y = Random.Range(-maxDeviation, maxDeviation);
-                    Instantiate(flowers[randomItem], /*generator._branches[i]._end*/generator._extremities[i]._end + new Vector3(x, y, -0.5f), new Quaternion(0, 0, 0, 0));
+                    Instantiate(flowers[randomItem], generator._branches[i]._end + new Vector3(x, y, -0.5f), new Quaternion(0, 0, 0, 0));
                 }
-                yield return new WaitForSeconds(0.01f);
-           /* }*/
+                yield return new WaitForSeconds(0.05f);
+            }
         }
     }
 }
