1c344104631775c1567a5ba690dc2d2e1e87de7a
Ghanshaam <gsewnath@gmail.com>
2021-06-25 09:40:44 +0200

    Adaptive fog

--- Grow Tree/Assets/Scripts/CameraFollow.cs
+++ Grow Tree/Assets/Scripts/CameraFollow.cs
@@ -75,6 +75,8 @@ public class CameraFollow : MonoBehaviour
         {
             //transform.position = Vector3.SmoothDamp(transform.position, finishPosition.position, ref velocity, cameraSmoothing);
             transform.position = Vector3.Lerp(transform.position, finishPosition.position, Time.deltaTime);
+            RenderSettings.fogEndDistance += (transform.position.z - finishPosition.position.z) / 100f;
+            RenderSettings.fogStartDistance += (transform.position.z - finishPosition.position.z) / 100f;
         }
     }
     //}
@@ -89,7 +91,6 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 centerPos = GetCenterPoint();
         newPos = centerPos + cameraOffset;
-
         transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, cameraSmoothing);
     }
 
