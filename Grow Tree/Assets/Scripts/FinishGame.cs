5ef110eb2413e86a6590dccb9f156ae77d33a7a4
Ghanshaam <gsewnath@gmail.com>
2021-07-02 14:48:07 +0200

    Updated 12

--- Grow Tree/Assets/Scripts/FinishGame.cs
+++ Grow Tree/Assets/Scripts/FinishGame.cs
@@ -42,7 +42,7 @@ public class FinishGame : MonoBehaviour
             }
             generator._attractors = attractorsInRange;
             wonGame = true;
-            //generator._killRange = 0.1f;
+            generator._killRange = 0.3f;
             generator.alternate = false;
             winGame.Raise();
             generator._timeBetweenIterations = 0.05f;
